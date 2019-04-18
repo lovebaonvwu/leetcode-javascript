eaf6911687d865c0c98438a967c15a6896dc62a9
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-18 09:35:33 +0800

    27. Remove Element

--- remove-element.js
+++ remove-element.js
@@ -30,3 +30,20 @@ var removeElement = function(nums, val) {
 
     return slow;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; ++i) {
+        if (nums[i] != val) {
+            nums[index++] = nums[i];
+        }
+    }
+
+    return index;
+};
